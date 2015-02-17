208b24ba8cc10af718ec6f3a4bffa364138c9212
srossross <srossross@gmail.com>
2015-02-17 13:21:50 -0800

    ENH: updated runner to inlclude root

--- chalmers/config.py
+++ chalmers/config.py
@@ -9,12 +9,12 @@ dirs = appdirs.AppDirs('chalmers', 'srossross')
 def set_relative_dirs(root):
     'Set the application directory relative root'
     global dirs
-    dirs = appdirs.RelativeAppDirs(root)
+    dirs = appdirs.RelativeAppDirs(os.path.abspath(root))
 
 def main_logfile():
     return os.path.join(dirs.user_log_dir, 'chalmers.log')
 
 # Set the root config and log dirs from an environment variable
 if os.environ.get('CHALMERS_ROOT'):
-    CHALMERS_ROOT = os.path.abspath(os.environ['CHALMERS_ROOT'])
+    CHALMERS_ROOT = os.environ['CHALMERS_ROOT']
     set_relative_dirs(CHALMERS_ROOT)
--- chalmers/scripts/runner.py
+++ chalmers/scripts/runner.py
@@ -3,10 +3,20 @@ from chalmers.program import Program
 import logging
 from clyent.logs import setup_logging
 from chalmers import config
+from argparse import ArgumentParser
 logger = logging.getLogger('chalmers')
 cli_logger = logging.getLogger('cli-logger')
 
 def main():
+
+    parser = ArgumentParser()
+    parser.add_argument('--root', help='chalmers root config directory')
+    args = parser.parse_args()
+
+    if args.root:
+        config.set_relative_dirs(args.root)
+
+
     logfile = config.main_logfile()
     setup_logging(logger, logging.INFO, use_color=False, logfile=logfile, show_tb=True)
     name = sys.argv[1]
