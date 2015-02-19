6f190b4e0c35735f8d981c1e94b43445ca45d286
srossross <srossross@gmail.com>
2015-02-19 09:01:42 -0800

    FIX: Close FD

--- chalmers/utils/file_echo.py
+++ chalmers/utils/file_echo.py
@@ -10,11 +10,7 @@ class FileEcho(Thread):
     def __init__(self, filename, stream):
         self.filename = filename
         self.stream = stream
-        self.fd = open(filename, 'a+', 0)
-        self.fd.seek(0, os.SEEK_END)
-        self.pos = self.fd.tell()
         self._running = True
-
         Thread.__init__(self, name='file_echo')
         self.daemon = True
 
@@ -22,15 +18,22 @@ class FileEcho(Thread):
         self._running = False
 
     def run(self):
-        while self._running:
-            if self.pos <= os.fstat(self.fd.fileno()).st_size:
-                self.fd.seek(self.pos)
-                data = self.fd.read().decode('utf-8')
-                self.pos = self.fd.tell()
-                try:
-                    self.stream.write(data)
-                except TypeError as err:
-                    log.exception(err)
-            else:
-                time.sleep(1)
+
+        with open(self.filename, 'a+', 1) as fd:
+            fd.seek(0, os.SEEK_END)
+            pos = fd.tell()
+
+            while self._running:
+                if pos <= os.fstat(fd.fileno()).st_size:
+                    fd.seek(pos)
+                    data = fd.read()
+                    if isinstance(data, bytes):
+                        data = data.decode('utf-8')
+                    pos = fd.tell()
+                    try:
+                        self.stream.write(data)
+                    except TypeError as err:
+                        log.exception(err)
+                else:
+                    time.sleep(1)
 
