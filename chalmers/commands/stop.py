26e1471d4375f22d484817857ee1e4f4d090d9df
srossross <srossross@gmail.com>
2015-02-18 16:04:51 -0800

    ENH: added --force option to stop command

--- chalmers/commands/stop.py
+++ chalmers/commands/stop.py
@@ -27,13 +27,28 @@ def main(args):
     else:
         programs = [Program(name) for name in args.names]
 
+    already_stopped_programs = [p.name for p in programs if not p.is_running]
+    if already_stopped_programs:
+        print("The programs '%s' are already stopped" % "', '".join(already_stopped_programs))
+
+    programs = [p for p in programs if p.is_running]
+
+    if len(programs):
+        print("Stopping programs %s" % ', '.join([p.name for p in programs]))
+        print("")
+    else:
+        print("No programs to stop")
+        return
+
     for prog in programs:
         if prog.is_running:
             print("Stopping program %-25s ... " % prog.name[:25], end=''); sys.stdout.flush()
             try:
-                prog.stop()
+                prog.stop(args.force)
             except errors.StateError as err:
                 log.error(err.message)
+            except errors.ConnectionError as err:
+                print_colors("[  {=ERROR!c:red}  ] %s (use --force to force stop the program)" % err.message)
             else:
                 print_colors("[  {=OK!c:green}  ]")
         else:
@@ -73,8 +88,14 @@ def add_parser(subparsers):
                                    description=__doc__,
                                    formatter_class=RawDescriptionHelpFormatter)
 
-    parser.add_argument('names', nargs='*', metavar='PROG')
-    parser.add_argument('-a', '--all', action='store_true')
+    parser.add_argument('names', nargs='*', metavar='PROG',
+                        help='Name of program')
+    parser.add_argument('-a', '--all', action='store_true',
+                        help='Select all programs')
+    parser.add_argument('--force', action='store_true',
+                        help='Force kill a program (stopsignal will be ignored)'
+                        )
+
     parser.set_defaults(main=main)
 
     parser = subparsers.add_parser('pause',
@@ -82,8 +103,10 @@ def add_parser(subparsers):
                                       description=__doc__,
                                       formatter_class=RawDescriptionHelpFormatter)
 
-    parser.add_argument('names', nargs='*', metavar='PROG')
-    parser.add_argument('-a', '--all', action='store_true')
+    parser.add_argument('names', nargs='*', metavar='PROG',
+                        help='Name of program')
+    parser.add_argument('-a', '--all', action='store_true',
+                        help='Select all programs')
 
     parser.set_defaults(main=pause_main)
 
@@ -92,7 +115,9 @@ def add_parser(subparsers):
                                       description=__doc__,
                                       formatter_class=RawDescriptionHelpFormatter)
 
-    parser.add_argument('names', nargs='*', metavar='PROG')
-    parser.add_argument('-a', '--all', action='store_true')
+    parser.add_argument('names', nargs='*', metavar='PROG',
+                        help='Name of program')
+    parser.add_argument('-a', '--all', action='store_true',
+                        help='Select all programs')
 
     parser.set_defaults(main=unpause_main)
