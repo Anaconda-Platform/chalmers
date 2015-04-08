d6ba01054c0782dbd9b2d93022f4f95967e96466
srossross <srossross@gmail.com>
2015-04-08 15:47:43 -0500

    ref removed old code

--- chalmers/__init__.py
+++ chalmers/__init__.py
@@ -4,17 +4,3 @@ try:
 except ImportError:
     __version__ = '0.3'
 
-import signal
-
-defaults = {
-            'retries': 3,
-            'exitcodes': [0],
-            'startsecs': 10,
-            'stopwaitsecs': 10,
-            'stopsignal': signal.SIGTERM,
-            }
-
-def make_program_data(data):
-    new_data = defaults.copy()
-    new_data.update(data)
-    return new_data
