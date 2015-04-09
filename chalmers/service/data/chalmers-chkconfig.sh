476ca049b4a523570fd3ffa9dc79da31fd6eaf5a
srossross <srossross@gmail.com>
2015-04-09 09:50:36 -0500

    added syslog logging

--- chalmers/service/data/chalmers-chkconfig.sh
+++ chalmers/service/data/chalmers-chkconfig.sh
@@ -9,15 +9,18 @@ LAUNCH="{launch}"
 case "$1" in
     start)
         $LAUNCH -c "$PYTHON $CHALMERS start --all"
+        logger  -t chalmers "init.d: start exitcode=$?"
         ;;
     stop)
         $LAUNCH -c "$PYTHON $CHALMERS stop --all"
+        logger  -t chalmers "init.d: stop exitcode=$?"
         ;;
     status)
         $LAUNCH -c "$PYTHON $CHALMERS list"
         ;;
     restart)
         $LAUNCH -c "$PYTHON $CHALMERS restart --all"
+        logger  -t chalmers "init.d: restart exitcode=$?"
         ;;
     *)
         echo "Usage:  {{start|stop|status|restart}}"
