c80781a1d567fb5d141878f4b8ec73bbdf4d82c3
srossross <srossross@gmail.com>
2015-10-01 10:55:33 -0700

    added versioneer

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
 import os
+import versioneer
 
 install_requires = ['psutil>=2.1.3', 'clyent', 'pyyaml']
 
@@ -7,17 +8,12 @@ if os.name == 'nt':
     install_requires.append('pywin32')
 
 
-ctx = {}
-try:
-    with open('chalmers/_version.py') as fd:
-        exec(open('chalmers/_version.py').read(), ctx)
-    version = ctx.get('__version__', 'dev')
-except IOError:
-    version = 'dev'
-
 setup(
     name='chalmers',
-    version=version,
+
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+
     author='Continuum Analytics',
     author_email='srossross@gmail.com',
     url='http://github.com/binstar/chalmers',
